b7b5ea11ba50551b59b74893b7e09e96bab07717
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-12-18 16:03:33 +0200

    XIP IO in use.

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,11 @@
+// Note that for Facebook authentication to work, this must be the same as in Facebook app settings.
+var SERVER_IP = "10.90.135.95";
+
 // Embeds the web page as the application.
 angular.module('starter', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    var url = "http://www.10.90.135.95.xip.io:8080";
+    var url = "http://www." + SERVER_IP + ".xip.io:8080";
     var ref = window.open(url, "_self");
   });
 });
